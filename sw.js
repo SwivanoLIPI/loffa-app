8e5c6bf297261f454f2cecf99d8cfad05e464a0c
swivano <67215234+SwivanoLIPI@users.noreply.github.com>
2025-11-17 21:50:34 +0700

    Update sw.js
    perubahan besar

--- sw.js
+++ sw.js
@@ -1,39 +1,45 @@
 const CACHE_NAME = 'loffa-cache-v1';
+
+// Prefix path GitHub Pages
+const BASE = '/loffa-app';
+
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/all.css',
-  '/assets/chart.js',
-  '/assets/mediapipe/drawing_utils.js',
-  '/assets/mediapipe/face_mesh/face_mesh.js',
-  '/assets/mediapipe/face_mesh/face_mesh.binarypb',
-  '/assets/mediapipe/face_mesh/face_mesh_solution_packed_assets.data',
-  '/assets/mediapipe/face_mesh/face_mesh_solution_packed_assets_loader.js',
-  '/assets/mediapipe/face_mesh/face_mesh_solution_simd_wasm_bin.data',
-  '/assets/mediapipe/face_mesh/face_mesh_solution_simd_wasm_bin.js',
-  '/assets/mediapipe/face_mesh/face_mesh_solution_simd_wasm_bin.wasm',
-  '/assets/mediapipe/face_mesh/face_mesh_solution_wasm_bin.js',
-  '/assets/mediapipe/face_mesh/face_mesh_solution_wasm_bin.wasm',
-  '/webfonts/fa-brands-400.ttf',
-  '/webfonts/fa-brands-400.woff2',
-  '/webfonts/fa-regular-400.ttf',
-  '/webfonts/fa-regular-400.woff2',
-  '/webfonts/fa-solid-900.ttf',
-  '/webfonts/fa-solid-900.woff2'
-  // Tambah jika ada file lain
+  `${BASE}/`,
+  `${BASE}/index.html`,
+  `${BASE}/all.css`,
+  `${BASE}/assets/chart.js`,
+  `${BASE}/assets/mediapipe/drawing_utils.js`,
+  `${BASE}/assets/mediapipe/face_mesh/face_mesh.js`,
+  `${BASE}/assets/mediapipe/face_mesh/face_mesh.binarypb`,
+  `${BASE}/assets/mediapipe/face_mesh/face_mesh_solution_packed_assets.data`,
+  `${BASE}/assets/mediapipe/face_mesh/face_mesh_solution_packed_assets_loader.js`,
+  `${BASE}/assets/mediapipe/face_mesh/face_mesh_solution_simd_wasm_bin.data`,
+  `${BASE}/assets/mediapipe/face_mesh/face_mesh_solution_simd_wasm_bin.js`,
+  `${BASE}/assets/mediapipe/face_mesh/face_mesh_solution_simd_wasm_bin.wasm`,
+  `${BASE}/assets/mediapipe/face_mesh/face_mesh_solution_wasm_bin.js`,
+  `${BASE}/assets/mediapipe/face_mesh/face_mesh_solution_wasm_bin.wasm`,
+  `${BASE}/webfonts/fa-brands-400.ttf`,
+  `${BASE}/webfonts/fa-brands-400.woff2`,
+  `${BASE}/webfonts/fa-regular-400.ttf`,
+  `${BASE}/webfonts/fa-regular-400.woff2`,
+  `${BASE}/webfonts/fa-solid-900.ttf`,
+  `${BASE}/webfonts/fa-solid-900.woff2`
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+  let requestURL = event.request.url;
 
+  // Re-route to subfolder when served
+  if (requestURL.includes('/loffa-app/')) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(response => response || fetch(event.request))
+    );
+  }
 });
